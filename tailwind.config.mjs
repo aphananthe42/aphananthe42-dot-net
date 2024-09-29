4f953048e8ae19c16bfefa529b090ac19556260d
aphananthe42 <aphananthe42@gmail.com>
2024-09-29 23:18:44 +0900

    fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,13 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				roboto: ['Roboto', 'sans-serif'],
-				notosansjp: ['Noto Sans JP Variable', 'sans-serif'],
+				roboto: ['Roboto', ...defaultTheme.fontFamily.serif],
+				notosansjp: ['Noto Sans JP Variable', ...defaultTheme.fontFamily.serif],
 			},
 		},
 	},
