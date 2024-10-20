667ea6f2267cd01f6127597bfceb8914a3130301
aphananthe42 <aphananthe42@gmail.com>
2024-10-20 22:15:35 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,26 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import rlc from 'remark-link-card'
+import rehypeRaw from 'rehype-raw'
+import rehypeExternalLinks from 'rehype-external-links'
 
 export default defineConfig({
   site: "https://aphananthe42.net",
-  integrations: [sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind()],
+  markdown: {
+    remarkPlugins: [
+      [
+        rlc,
+        { shortenUrl: true },
+      ],
+    ],
+    rehypePlugins: [
+      rehypeRaw,
+      [
+        rehypeExternalLinks,
+        { target: '_blank' },
+      ],
+    ],
+  },
 });
