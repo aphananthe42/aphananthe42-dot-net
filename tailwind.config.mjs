6d9cd510647ced4e97ecf3a23c6630de2abddf42
aphananthe42 <aphananthe42@gmail.com>
2024-10-20 11:17:31 +0900

    fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,6 +9,21 @@ export default {
 				roboto: ['Roboto', ...defaultTheme.fontFamily.serif],
 				notosansjp: ['Noto Sans JP', ...defaultTheme.fontFamily.serif],
 			},
+			animation: {
+                "text-focus-in": "text-focus-in 1s cubic-bezier(0.550, 0.085, 0.680, 0.530) both"
+            },
+            keyframes: {
+                "text-focus-in": {
+                    "0%": {
+                        filter: "blur(12px)",
+                        opacity: "0"
+                    },
+                    to: {
+                        filter: "blur(0)",
+                        opacity: "1"
+                    }
+                }
+            }
 		},
 	},
 	plugins: [
