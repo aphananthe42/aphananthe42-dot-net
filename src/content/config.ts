52e7c8be55e669e3be9832d94ef81ecc91929942
aphananthe42 <contact@aphananthe42.net>
2025-03-25 00:36:27 +0900

    Remove tech-blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,23 +1,13 @@
-import { z, defineCollection } from 'astro:content';
-
-const techBlogCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        tags: z.array(z.string()),
-        date: z.date(),
-    }),
-});
+import { z, defineCollection } from "astro:content";
 
 const diaryCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        date: z.date(),
-    }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		date: z.date(),
+	}),
 });
 
 export const collections = {
-    techBlog: techBlogCollection,
-    diary: diaryCollection,
+	diary: diaryCollection,
 };
