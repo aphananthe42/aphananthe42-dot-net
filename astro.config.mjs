8966314a91cd093ca6de7b2d09b97cfe9692f244
aphananthe42 <contact@aphananthe42.net>
2025-10-16 23:14:43 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import rlc from 'remark-link-card'
 import rehypeRaw from 'rehype-raw'
 import rehypeExternalLinks from 'rehype-external-links'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: "https://aphananthe42.net",
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap()],
   markdown: {
     remarkPlugins: [
       [
@@ -22,5 +22,8 @@ export default defineConfig({
         { target: '_blank' },
       ],
     ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
