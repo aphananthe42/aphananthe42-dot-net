af823ceef30fa9f0e989a9331b7983ea3cdf696e
aphananthe42 <contact@aphananthe42.net>
2025-10-17 00:34:38 +0900

    fix: content collection -> content layer

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { z, defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
 
 const diaryCollection = defineCollection({
-	type: "content",
+	loader: glob({ pattern: "**/*.md", base: "./src/content/diary" }),
 	schema: z.object({
 		title: z.string(),
 		date: z.date(),
